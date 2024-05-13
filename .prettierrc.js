2b02cba0092aba38312bd1525151c86b12cf59a2
ChaeyoonWoo <heeni1004@gmail.com>
2024-05-13 15:37:08 +0900

    feat: search기능 점검

---
+++ .prettierrc.js
@@ -0,0 +1,9 @@
+const config = {
+    singleQuote: false,
+    semi: true,
+    useTabs: false,
+    tabWidth: 2,
+    printWidth: 120,
+  };
+module.exports = config;
+  
--- src/components/Todo/TodoAppVer2.js
+++ src/components/Todo/TodoAppVer2.js
@@ -8,13 +8,11 @@ export default function TodoApp() {
   const [inputText, setInputText] = useState("");
   const [searchText, setSearchText] = useState(""); // 검색어 상태 추가
 
-
   useEffect(() => {
     if (todoList && todoList.length > 0) {
       localStorage.setItem("todoList", JSON.stringify(todoList));
     }
   }, [todoList]);
-
 
   useEffect(() => {
     const storedTodoList = JSON.parse(localStorage.getItem("todoList"));
@@ -24,7 +22,6 @@ export default function TodoApp() {
       setTodoList(storedTodoList);
     }
   }, []);
-
 
   const handleInputChange = (event) => {
     setInputText(event.target.value);
@@ -38,11 +35,10 @@ export default function TodoApp() {
   const handleSearch = () => {
     // 검색 버튼을 클릭했을 때 실행되는 함수
     // 검색어에 해당하는 할 일 항목만 보여주도록 todoList를 필터링합니다.
-    const filteredTodos = todoList.filter(todo => todo.text.includes(searchText));
+    const filteredTodos = todoList.filter((todo) => todo.text.includes(searchText));
     // 필터링된 할 일 목록을 설정합니다.
     setTodoList(filteredTodos);
   };
-
 
   const addTodo = () => {
     const newTodo = {
@@ -53,21 +49,28 @@ export default function TodoApp() {
     setInputText("");
   };
 
+  const handleItemClick = (index) => {
+    const newTodoList = [...todoList];
+    newTodoList.splice(index, 1);
+    setTodoList(newTodoList);
+  };
+
   return (
     <div
       style={{
         display: "flex",
         alignItems: "center",
-        justifyContent: "center",
+        // justifyContent: "center",
         flexDirection: "column",
-        margin: 100
+        margin: 100,
       }}
     >
       <div style={{ textAlign: "center" }}>
         <h1>Todo App</h1>
-        <div style={{marginTop: 50, marginBottom: 20}}>
+        <div style={{ marginTop: 50, marginBottom: 20 }}>
           <input
-            style={{ backgroundColor: activeColor, width: 500}}
+            type="text"
+            style={{ backgroundColor: activeColor, width: 500 }}
             placeholder="입력"
             onChange={handleInputChange}
             value={inputText}
@@ -75,14 +78,14 @@ export default function TodoApp() {
           <button onClick={addTodo}>입력</button>
         </div>
         {/* 검색어 입력창 */}
-        <div style={{marginBottom: 50}}>
+        <div style={{ marginBottom: 50 }}>
           <input
             type="text"
             placeholder="검색"
             onChange={handleSearchChange}
             value={searchText} // 검색어 상태와 연결
           />
-          <button onClick={handleSearch} >검색</button>
+          <button onClick={handleSearch}>검색</button>
         </div>
       </div>
       <div style={{ display: "flex" }}>
@@ -108,15 +111,16 @@ export default function TodoApp() {
           display: "flex",
           flexDirection: "column",
           alignItems: "center",
-          margin: 50
+          margin: 50,
         }}
       >
         <h3>Todo Items</h3>
         <div>
           {todoList
             // .filter((todo) => todo.text.includes(searchText)) // 검색어로 필터링
-            .map((todo) => (
+            .map((todo, index) => (
               <div
+                key={index}
                 style={{
                   backgroundColor: todo.color,
                   width: 700,
@@ -126,6 +130,7 @@ export default function TodoApp() {
                   justifyContent: "center",
                   alignItems: "center",
                 }}
+                onClick={() => handleItemClick(index)}
               >
                 {todo.text}
               </div>
